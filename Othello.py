f23d3989c0764ddb8a16868cf80d405389ba6d1f
Bennie van Eeden <christiaanben@gmail.com>
2018-05-14 00:39:40 +0200

    ai vs ai play, player vs ai play

--- Othello.py
+++ Othello.py
@@ -8,36 +8,51 @@ import random
 
 
 class AI:
-
-    def __init__(self, player, n_hidden_nodes=16, weight1=None, weight2=None, weight3=None):
-        if weight1 == None:
-            print('No weight 1 supplied')
+    def __init__(self, player=None, name=None, n_hidden_nodes=16, weight1=None, weight2=None, weight3=None):
+        if weight1 is None:
             self.weight1 = np.zeros((n_hidden_nodes-1, 64))
             for i in range(len(self.weight1)):
                 for j in range(len(self.weight1[i])):
                     self.weight1[i, j] = random.random()
         else:
             self.weight1 = weight1
-        self.weight2 = np.zeros((n_hidden_nodes-1, n_hidden_nodes))
-        for i in range(len(self.weight2)):
-            for j in range(len(self.weight2[i])):
-                self.weight2[i, j] = random.random()
-        self.weight3 = np.ones((64,n_hidden_nodes))
-        for i in range(len(self.weight3)):
-            for j in range(len(self.weight3[i])):
-                self.weight3[i, j] = random.random()
+        if weight2 is None:
+            self.weight2 = np.zeros((n_hidden_nodes-1, n_hidden_nodes))
+            for i in range(len(self.weight2)):
+                for j in range(len(self.weight2[i])):
+                    self.weight2[i, j] = random.random()
+        else:
+            self.weight2 = weight2
+        if weight3 is None:
+            self.weight3 = np.ones((64, n_hidden_nodes))
+            for i in range(len(self.weight3)):
+                for j in range(len(self.weight3[i])):
+                    self.weight3[i, j] = random.random()
+        else:
+            self.weight3 = weight3
+        self.player = player
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_player(self, player):
         self.player = player
 
+    def get_player(self):
+        return self.player
 
+    def get_weights(self):
+        return self.weight1, self.weight2, self.weight3
 
     def get_next_move(self, board):
-        input_layer = np.reshape(board, 64)
+        input_layer = np.reshape((self.player*board), 64)
         x = self.sigmoid(np.dot(self.weight1, input_layer))
         x = np.append(x, 1)
         y = self.sigmoid(np.dot(self.weight2, x))
         y = np.append(y, 1)
         z = self.sigmoid(np.dot(self.weight3, y))
-        return np.argmax(z)
+        return int(np.argmax(z)/8), np.argmax(z) % 8
 
     def sigmoid(self, nodes):
         for i in range(len(nodes)):
@@ -60,6 +75,81 @@ class Othello:
             for j in range(8):
                 self.board[i][j] = board[i][j]
 
+    def play(self, ai, player=1):
+        ai.set_player(-player)
+
+        if player == -1:
+            if self.any_move(player):
+                self.display()
+                row, col = list(map(int, input('Input:').split(',')))
+                print('Player put a '+str(player)+' at ('+str(row)+','+str(col)+')')
+                if not self.check_move(row, col, player):
+                    print('AI wins')
+                    return
+                self.make_move(row, col, player)
+
+        moved = True
+        while moved:
+            moved = False
+            if self.any_move(ai.get_player()):
+                self.display()
+                row, col = ai.get_next_move(self.board)
+                print('AI put a '+str(ai.get_player())+' at ('+str(row)+','+str(col)+')')
+                if not self.check_move(row, col, ai.get_player())[0]:
+                    print('Player wins')
+                    return
+                moved = True
+                self.make_move(row, col, ai.get_player())
+            if self.any_move(player):
+                self.display()
+                row, col = list(map(int, input('Input:').split(',')))
+                print('Player put a '+str(player)+' at ('+str(row)+','+str(col)+')')
+                if not self.check_move(row, col, player):
+                    print('AI wins')
+                    return
+                self.make_move(row, col, player)
+        result = self.get_result()
+        if result[player] >= result[ai.get_player()]:
+            print('Player wins')
+        else:
+            print('AI wins')
+
+    def display(self):
+        print(self.board)
+
+    def play_self(self, player1, player2):
+        black = player2
+        white = player1
+        if random.random() < 0.5:
+            black = player1
+            white = player2
+        black.set_player(-1)
+        white.set_player(1)
+
+        moved = True
+        while moved:
+            moved = False
+            if self.any_move(black.get_player()):
+                row, col = black.get_next_move(self.board)
+                if not self.check_move(row, col, black.get_player())[0]:
+                    return white
+                moved = True
+                self.make_move(row, col, black.get_player())
+            if self.any_move(white.get_player()):
+                row, col = white.get_next_move(self.board)
+                if not self.check_move(row, col, white.get_player())[0]:
+                    return black
+                moved = True
+                self.make_move(row, col, white.get_player())
+        result = self.get_result()
+        if result[-1] > result[1]:
+            return black
+        return white
+
+    def get_result(self):
+        unique, counts = np.unique(self.board, return_counts=True)
+        return dict(zip(unique, counts))
+
     def get_board(self):
         return self.board
 
@@ -80,7 +170,7 @@ class Othello:
                     opposite_player_adjacent = True
             except:
                 pass
-        if not(opposite_player_adjacent):
+        if not opposite_player_adjacent:
             return "Error - Tile not adjacent to the opponent's tile"
 
         # Check if adjacent tile closes on both ends
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Othello import Othello
 from Othello import AI
+import numpy as np
 
 board_empty = [[0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,8 +20,27 @@ board_test = [[0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0]]
 
-game = Othello()
-print(game.get_board())
-player1 = AI(1)
-player2 = AI(2)
-print(player1.get_next_move(game.get_board()))
+
+ai = []
+n_ai = 1000
+score = np.zeros(n_ai, dtype='int')
+for i in range(n_ai):
+    ai.append(AI(name=i))
+for i in range(n_ai):
+    for j in range((i+1), n_ai):
+        game = Othello()
+        winner = game.play_self(ai[i], ai[j])
+        score[winner.get_name()] += 1
+final_winner = np.argmax(score)
+print('AI-'+str(final_winner)+' won '+str(score[final_winner])+' out of '+str(n_ai-1)+' ('+str(score[final_winner]/(n_ai-1)*100)+'%) games.')
+weight1, weight2, weight3 = ai[final_winner].get_weights()
+np.save('weight1.npy', weight1)
+np.save('weight2.npy', weight2)
+np.save('weight3.npy', weight3)
+
+# w1 = np.load('weight1.npy')
+# w2 = np.load('weight2.npy')
+# w3 = np.load('weight3.npy')
+# ai = AI(player=1, weight1=w1, weight2=w2, weight3=w3)
+# game = Othello()
+# game.play(ai, -1)
