7d4cd12389f6633c0268ef14ca4d473fdff25b77
Bennie van Eeden <christiaanben@gmail.com>
2018-05-15 15:22:56 +0200

    save training progress

--- Othello.py
+++ Othello.py
@@ -8,9 +8,9 @@ import random
 
 
 class AI:
-    def __init__(self, player=None, name=None, n_hidden_nodes=64, weight1=None, weight2=None):
+    def __init__(self, player=None, name=None, n_hidden_nodes=100, weight1=None, weight2=None):
         if weight1 is None:
-            self.weight1 = np.zeros((n_hidden_nodes, 64))
+            self.weight1 = np.zeros((n_hidden_nodes, 2*64))
             for i in range(len(self.weight1)):
                 for j in range(len(self.weight1[i])):
                     self.weight1[i, j] = random.random()*2 - 1
@@ -38,17 +38,27 @@ class AI:
     def get_weights(self):
         return self.weight1, self.weight2
 
-    def get_next_move(self, board):
+    def set_weights(self, weight1, weight2):
+        self.weight1 = weight1
+        self.weight2 = weight2
+
+    def get_next_move(self, board, available_moves):
         board = (self.player*board)
-        value_per_quadrant = Othello.quadrant_value(board)
-        board_x = rotate(np.argmax(value_per_quadrant))
-        input_layer = np.reshape(board, 64)
+        n_rotations = np.argmax(Othello.quadrant_value(board))
+        rotated_board = Othello.rotate(board, n_rotations)
+        rotated_moves = Othello.rotate(available_moves, n_rotations)
+        input_layer = np.reshape(rotated_board, 64)
+        input_layer = np.append(input_layer, np.reshape(rotated_moves, 64))
         x = self.sigmoid(np.dot(self.weight1, input_layer))
         x = np.append(x, 1)
         y = self.sigmoid(np.dot(self.weight2, x))
-        return int(np.argmax(y)/8), np.argmax(y) % 8
+        rotated_output = np.reshape(y, (8, 8))
+        output = Othello.rotate(rotated_output, (4-n_rotations) % 4)
+        index = np.unravel_index(output.argmax(), output.shape)
+        return index
 
-    def sigmoid(self, nodes):
+    @staticmethod
+    def sigmoid(nodes):
         for i in range(len(nodes)):
             nodes[i] = 1/(1+np.exp(nodes[i]))
         return nodes
@@ -62,38 +72,30 @@ class AI:
         # 4. Inverse weight
         weight1 = self.weight1
         weight2 = self.weight2
-        max = np.max(weight1)
         for i in range(len(weight1)):
             for j in range(len(weight1[i])):
                 prob = random.random()
                 if prob < 0.002:
-                    weight1[i, j] = (random.random()-0.5)*max*.8
+                    weight1[i, j] = random.random()
                 elif prob < 0.004:
-                    weight1[i, j] = weight1[i, j] * (random.random()+0.4)
+                    weight1[i, j] = weight1[i, j] * (random.random()-0.5)
                 elif prob < 0.006:
                     weight1[i, j] = weight1[i, j] + random.random()*2 - 1
                 elif prob < 0.008:
                     weight1[i, j] = -1 * weight1[i, j]
-                if weight1[i, j] > 1000000:
-                    weight1[i, j] = 1000000
-                if weight1[i, j] < -1000000:
-                    weight1[i, j] = -1000000
-        max = np.max(weight2)
+                weight1[i, j] = max(-10.0, min(10.0, weight1[i, j]))
         for i in range(len(weight2)):
             for j in range(len(weight2[i])):
                 prob = random.random()
                 if prob < 0.002:
-                    weight2[i, j] = (random.random()-0.5)*max*.8
+                    weight2[i, j] = random.random()
                 elif prob < 0.004:
-                    weight2[i, j] = weight2[i, j] * (random.random()+0.4)
+                    weight2[i, j] = weight2[i, j] * (random.random()-0.5)
                 elif prob < 0.006:
                     weight2[i, j] = weight2[i, j] + random.random()*2 - 1
                 elif prob < 0.008:
                     weight2[i, j] = -1 * weight2[i, j]
-                if weight2[i, j] > 1000000:
-                    weight2[i, j] = 1000000
-                if weight2[i, j] < -1000000:
-                    weight2[i, j] = -1000000
+                weight2[i, j] = max(-10.0, min(10.0, weight2[i, j]))
         return weight1, weight2
 
 
@@ -114,11 +116,27 @@ class Othello:
                 self.board[i][j] = board[i][j]
 
     @staticmethod
+    def rotate(board, n_rotations):
+        for n in range(n_rotations):
+            rotated = zip(*board[::-1])
+            board = np.array(list(rotated))
+        return board
+
+    @staticmethod
     def quadrant_value(board):
-        q = [[],[],[],[]]
+        '''
+        Quadrants look like this:
+        0|3
+        1|2
+        so that i can rotate easily
+        :param board:
+        :return:
+        '''
+        q = [[], [], [], []]
         for i in range(8):
             for j in range(8):
-                index = 2*int(i / 4) + int(j / 4)
+                row, col = int(i / 4), int(j / 4)
+                index = int(np.abs(row - 3*col))
                 q[index].append(board[i, j])
         value = np.zeros(4)
         for i in range(4):
@@ -144,9 +162,10 @@ class Othello:
         moved = True
         while moved:
             moved = False
-            if self.any_move(ai.get_player()):
+            available, moves = self.any_move(ai.get_player())
+            if available:
                 self.display()
-                row, col = ai.get_next_move(self.board)
+                row, col = ai.get_next_move(self.board, moves)
                 print('AI put a '+str(ai.get_player())+' at ('+str(row)+','+str(col)+')')
                 if not self.check_move(row, col, ai.get_player())[0]:
                     print('Player wins')
@@ -182,8 +201,9 @@ class Othello:
         moved = True
         while moved:
             moved = False
-            if self.any_move(black.get_player()):
-                row, col = black.get_next_move(self.board)
+            available, moves = self.any_move(black.get_player())
+            if available:
+                row, col = black.get_next_move(self.board, moves)
                 if display_board:
                     self.display()
                     print('AI put a ' + str(black.get_player()) + ' at (' + str(row) + ',' + str(col) + ')')
@@ -191,8 +211,9 @@ class Othello:
                     return white
                 moved = True
                 self.make_move(row, col, black.get_player())
-            if self.any_move(white.get_player()):
-                row, col = white.get_next_move(self.board)
+            available, moves = self.any_move(white.get_player())
+            if available:
+                row, col = white.get_next_move(self.board, moves)
                 if display_board:
                     self.display()
                     print('AI put a ' + str(white.get_player()) + ' at (' + str(row) + ',' + str(col) + ')')
@@ -281,6 +302,9 @@ class Othello:
                 if self.board[tile[0], tile[1]] == 0:
                     break
                 tile = np.add(tile, delta)
+                if not (-1 < tile[0] < 8 and -1 < tile[1] < 8):
+                    tile = np.subtract(tile, delta)
+                    break
             if self.board[tile[0], tile[1]] == player:
                 valid = True
         if not valid:
@@ -289,8 +313,11 @@ class Othello:
         return True, 'Success'
 
     def any_move(self, player):
+        available = False
+        moves = np.zeros((8, 8))
         for i in range(8):
             for j in range(8):
                 if self.check_move(i, j, player)[0]:
-                    return True
-        return False
+                    available = True
+                    moves[i, j] = 1
+        return available, moves
--- train.py
+++ train.py
@@ -2,8 +2,9 @@ from Othello import Othello
 from Othello import AI
 import numpy as np
 
-n_ai = 500
-n_winners = 5
+n_ai = 5000
+n_winners = 50
+restart = False
 ai_winners = None
 
 generation = 0
@@ -16,21 +17,28 @@ while True:
         if ai_winners is not None:
             weight1, weight2 = ai_winners[int(i/(n_ai/n_winners))].mutate()
             if i % n_winners == 0:
-                weight1, weight2 = ai_winners[int(i / (n_ai / n_winners))].get_weight()
+                weight1, weight2 = ai_winners[int(i / (n_ai / n_winners))].get_weights()
             ai.append(AI(name=i, weight1=weight1, weight2=weight2))
         else:
             ai.append((AI(name=i)))
+    prev_percentage = 0
+    print('Complete: ', end='', flush=True)
     for i in range(n_ai):
+        percentage = int(i/n_ai*100)
+        [print('#', end='', flush=True) for x in range(percentage-prev_percentage)]
+        prev_percentage = percentage
         for j in range((i+1), n_ai):
             game = Othello()
             winner = game.play_self(ai[i], ai[j])
             score[winner.get_name()] += 1
+    print('| 100%')
     winners = score.argsort()[-n_winners:][::-1]
     ai_winners = []
     for winner in winners:
         ai_winners.append(ai[winner])
     final_winner = np.argmax(score)
     print('Ai-{} won {} out of {} ({:.2f}%) games.'.format(final_winner, score[final_winner], n_ai-1, score[final_winner]/(n_ai-1)*100))
-    weight1, weight2 = ai[final_winner].get_weights()
-    np.save('weight'+str(generation)+'-1.npy', weight1)
-    np.save('weight'+str(generation)+'-2.npy', weight2)
+    for i in range(len(ai_winners)):
+        weight1, weight2 = ai_winners[i].get_weights()
+        np.save('weights\weight-Gen{:02d}-{:02d}-1.npy'.format(generation, i), weight1)
+        np.save('weights\weight-Gen{:02d}-{:02d}-2.npy'.format(generation, i), weight2)
